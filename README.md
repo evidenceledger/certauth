f5a43315b30e128b6142a420b93c20ba119fdb5a
Jesus Ruiz <hesus.ruiz@gmail.com>
2025-02-13 16:48:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# OpenID Provider for authenticating users with an eIDAS digital certificate
+
+This server is based on code from [ORY Fosite example](https://github.com/ory/fosite-example) for the OpenID Provider functionality. The code maintains the copyright and attributions, but it removes unneccesary code to help keep this server simple and understandable.
+
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/evidenceledger/certauth/authorizationserver"
+	"github.com/evidenceledger/certauth/oauth2client"
+	"github.com/evidenceledger/certauth/resourceserver"
+	goauth "golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+// A valid oauth2 client (check the store) that additionally requests an OpenID Connect id token
+var clientConf = goauth.Config{
+	ClientID:     "my-client",
+	ClientSecret: "foobar",
+	RedirectURL:  "https://cert.mycredential.eu/callback",
+	Scopes:       []string{"photos", "openid", "offline"},
+	Endpoint: goauth.Endpoint{
+		TokenURL: "https://cert.mycredential.eu/oauth2/token",
+		AuthURL:  "https://cert.mycredential.eu/oauth2/auth",
+	},
+}
+
+// The same thing (valid oauth2 client) but for using the client credentials grant
+var appClientConf = clientcredentials.Config{
+	ClientID:     "my-client",
+	ClientSecret: "foobar",
+	Scopes:       []string{"fosite"},
+	TokenURL:     "https://cert.mycredential.eu/oauth2/token",
+}
+
+// Samle client as above, but using a different secret to demonstrate secret rotation
+var appClientConfRotated = clientcredentials.Config{
+	ClientID:     "my-client",
+	ClientSecret: "foobaz",
+	Scopes:       []string{"fosite"},
+	TokenURL:     "https://cert.mycredential.eu/oauth2/token",
+}
+
+func main() {
+	// ### oauth2 server ###
+	authorizationserver.RegisterHandlers() // the authorization server (fosite)
+
+	// ### oauth2 client ###
+	http.HandleFunc("/", oauth2client.HomeHandler(clientConf)) // show some links on the index
+
+	// the following handlers are oauth2 consumers
+	http.HandleFunc("/client", oauth2client.ClientEndpoint(appClientConf))            // complete a client credentials flow
+	http.HandleFunc("/client-new", oauth2client.ClientEndpoint(appClientConfRotated)) // complete a client credentials flow using rotated secret
+	http.HandleFunc("/owner", oauth2client.OwnerHandler(clientConf))                  // complete a resource owner password credentials flow
+	http.HandleFunc("/callback", oauth2client.CallbackHandler(clientConf))            // the oauth2 callback endpoint
+
+	// ### protected resource ###
+	http.HandleFunc("/protected", resourceserver.ProtectedEndpoint(appClientConf))
+
+	port := "3846"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+
+	fmt.Println("Please open your webbrowser at https://cert.mycredential.eu")
+	// _ = exec.Command("open", "http://localhost:"+port).Run()
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
