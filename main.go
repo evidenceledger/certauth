55631910e41d5eae9b91587c807fbd6262d7f539
Jesus Ruiz <hesus.ruiz@gmail.com>
2025-08-31 16:44:43 +0000

    Fix command line

--- main.go
+++ main.go
@@ -27,15 +27,15 @@ func main() {
 
 	// The URL and port for the CertAuth server, which is the OP url also
 	flag.StringVar(&certauthPort, "certauth-port", "8090", "Port for the main OP server")
-	flag.StringVar(&certauthURL, "certauth-url", "https://certauth.mycredential.eu", "URL for the CertAuth server")
+	flag.StringVar(&certauthURL, "certauth-url", "", "URL for the CertAuth server")
 
 	// The URL and port for the CertSec server, the one asking for the certificate via TLS client authentication
 	flag.StringVar(&certsecPort, "certsec-port", "8091", "Port for the CertSec server")
-	flag.StringVar(&certsecURL, "certsec-url", "https://certsec.mycredential.eu", "URL for the CertSec server")
+	flag.StringVar(&certsecURL, "certsec-url", "", "URL for the CertSec server")
 
 	// The URL and port for the Onboard server, the example RP
 	flag.StringVar(&onboardPort, "onboard-port", "8092", "Port for the Onboard server")
-	flag.StringVar(&onboardURL, "onboard-url", "http://localhost:8092", "URL for the Onboard server")
+	flag.StringVar(&onboardURL, "onboard-url", "", "URL for the Onboard server")
 
 	flag.Parse()
 
